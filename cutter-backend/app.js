2eacec38b7a3674a718f98901c7fc31192b48e43
Elcan Ali <elcan@div.edu.az>
2023-08-10 08:20:41 +0400

    add cors

--- cutter-backend/app.js
+++ cutter-backend/app.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-
+const cors = require('cors')
 const MealsRouter = require('./routes/main/Meals')
 const RestaurantRouter = require('./routes/main/restaurants')
 
+app.use(cors())
 app.use(express.json())
 app.use("/meals", MealsRouter)
 app.use("/restaurants", RestaurantRouter)
